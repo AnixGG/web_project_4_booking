2cef5dd1cd55a8e7a7bb9c752c42497848885aa2
AnixGG <g4zizullinn@yandex.ru>
2025-06-20 01:05:32 +0300

    fix middleware.ts
    Took 2 hours 0 minutes

---
+++ src/middleware.ts
@@ -0,0 +1,39 @@
+// middleware.ts (надежная версия для NextAuth v4)
+
+import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  const { pathname } = req.nextUrl;
+  if (token) {
+    console.log('Token found:', JSON.stringify(token, null, 2));
+  } else {
+    console.log('No token found.');
+  }
+  console.log('------------------------\n');
+  if (pathname.startsWith('/admin')) {
+    if (!token || token.role !== 'ADMIN') {
+      const url = new URL(`/`, req.url);
+      url.searchParams.set('error', 'access_denied');
+      return NextResponse.redirect(url);
+    }
+  }
+
+  if (pathname.startsWith('/auth')) {
+    if (token) {
+      const url = new URL('/', req.url);
+      return NextResponse.redirect(url);
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/admin/:path*', 
+    '/auth/:path*'
+  ],
+};
