e2a98f632fc38028080e75b632df2f0c99a2a1db
AnixGG <g4zizullinn@yandex.ru>
2025-06-19 18:39:14 +0300

    update middleware.ts for admin

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,22 @@
-export { default } from 'next-auth/middleware';
+import { withAuth, NextRequestWithAuth } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
+
+export default withAuth(
+  function middleware(request: NextRequestWithAuth) {
+    if (
+      request.nextUrl.pathname.startsWith('/admin') &&
+      request.nextauth.token?.role !== 'ADMIN'
+    ) {
+      return NextResponse.rewrite(new URL('/', request.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
 
 export const config = {
-  // защищаем все роуты, начинающиеся с /admin
   matcher: ['/admin/:path*'],
 };
